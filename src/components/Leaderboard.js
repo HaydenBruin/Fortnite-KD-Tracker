c78e8933e1ad0e4f0cddbdb703298e0c77eb4e6b
Hayden Bruin <bruinhayden@gmail.com>
2018-05-03 01:23:18 +1200

    Continuing work on leaderboard structure

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -2,42 +2,85 @@ import React from 'react';
 import DefaultLayout from './DefaultLayout.js';
 
 class Leaderboard extends React.Component {
-    render() {
-        return (
-            <DefaultLayout>
-                <div className="section page">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-12 content">
-                                <h1>Your Group Title</h1>
-                                <p>A group of mighty warriors battling to be the best</p>
+    constructor(props) {
+        super(props);
+        this.state = {
+            apiurl: "http://fortnite.bruindev.com",
+            leaderboard: [],
+            hasLoaded: false
+        };
+        // LOAD PLATFORM DATA
+        fetch(this.state.apiurl + "/api/loadLeaderboard.php?leaderboard=" + this.props.match.params.leaderboard)
+            .then((resp) => resp.json())
+            .then(data => {
+                this.setState({ leaderboard: data, hasLoaded: true });
+                console.log(data);
+            }
+        )
+    }
 
-                                <table width="100%">
-                                    <thead>
-                                        <th>#</th>
-                                        <th>Player</th>
-                                        <th>Solo K/D</th>
-                                        <th>Duo K/D</th>
-                                        <th>Squad K/D</th>
-                                        <th>Overall K/D</th>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Mr Haydz</td>
-                                            <td>1.00</td>
-                                            <td>1.00</td>
-                                            <td>1.00</td>
-                                            <td>1.00</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+    render() {
+        if(this.state.hasLoaded)
+        {
+            return (
+                <DefaultLayout>
+                    <div className="section page">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-12 content">
+                                    <h1>Your Group Title</h1>
+                                    <p>A group of mighty warriors battling to be the best</p>
+
+                                    <table width="100%">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Player</th>
+                                                <th>Solo K/D</th>
+                                                <th>Duo K/D</th>
+                                                <th>Squad K/D</th>
+                                                <th>Overall K/D</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {this.state.leaderboard.players.map((field, i) => {
+                                                return <tr key={i}> 
+                                                        <td>{i}</td>
+                                                        <td>Mr Haydz</td>
+                                                        <td>{field['solo_kd']}</td>
+                                                        <td>{field['duo_kd']}</td>
+                                                        <td>{field['squad_kd']}</td>
+                                                        <td>{field['overall_kd']}</td>
+                                                    </tr>
+                                            })}
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </DefaultLayout>
-        );
+                </DefaultLayout>
+            );
+        }
+        else
+        {
+            return (
+                <DefaultLayout>
+                    <div className="section page">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-12 content">
+                                    <h1>Your Group Title</h1>
+                                    <p>A group of mighty warriors battling to be the best</p>
+
+                                    <h2>Loading leaderboard....</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </DefaultLayout>
+            );
+        }
     }
 }
 
---
+++ src/getAPIUrl.js
@@ -0,0 +1,3 @@
+export default function ApiURL() {
+    return "test";
+}
